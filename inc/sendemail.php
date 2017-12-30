b9dda17acb8ebb06e720f052f6a270b1ec03c17f
CassavaCrunch <kucingmangkal98@gmail.com>
2017-12-30 23:17:01 +0700

    perubahan

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,20 +1,20 @@
 <?php
 
-    $to = "backpiper.com@gmail.com";
+    $to = "naufalrocket98@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message from your BizPro";
+    $subject = "[Cassava Crunch] Pesan dari CassavaCrunch.github.io!";
 
     $fields = array();
-    $fields{"Fname"}    = "First Name";
-    $fields{"Lname"}    = "Last Name";
+    $fields{"Fname"}    = "Nama Depan";
+    $fields{"Lname"}    = "Nama Belakang";
     $fields{"email"}    = "Email";
-    $fields{"sub"}    = "Subject";
-    $fields{"message"}   = "Message";
-    
+    $fields{"sub"}    = "Subjek";
+    $fields{"message"}   = "Pesan";
 
-    $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $body = "Pesan yang Anda dapat:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
 
--- js/map-script.js
+++ js/map-script.js
@@ -5,7 +5,7 @@ function goMap () {
   if ($('#map').length) {
         // Styles a map in night mode.
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.925372, lng: -74.276544},
+          center: {lat: -7.747511, lng: 110.358888},
           zoom: 12,
           scrollwheel: false,
           styles: [ {
@@ -176,15 +176,15 @@ function goMap () {
           ]
         });
 
-		// To add the marker to the map 
+		// To add the marker to the map
 		  var image = 'images/logo/map.png';
 		  var beachMarker = new google.maps.Marker({
-		    position: {lat: 40.925372, lng: -74.276544},
+		    position: {lat: -7.747511, lng: 110.358888},
 		    map: map,
 		    icon: image,
 		    title:"BipPro Office!",
 		    infoWindow: {
-	            content: '<h6>Bizpro office</h6> <p>Gazipur,Dhaka</p>'
+	            content: '<h6>Cassava Crunch</h6> <p>Sleman, DIY</p>'
 	          }
 		  });
   };
